ed58ee43fe561706cf40f6fccb0c8d1442542951
AbiusX <git@abiusx.com>
2019-12-06 13:38:01 +0300

    New captcha, rotates letters individually.
    Lighter than previous version, removed M due to ambiguity, letters are rotated individually,
    letters are scaled first then distorted and rotated for better clarity.

--- PHP/purecaptcha.php
+++ PHP/purecaptcha.php
@@ -1,134 +1,107 @@
 <?php
-/**
+ /**
  * OWASP PureCaptcha
- * Generates simple CAPTCHAs without requiring any third party library.
- * @version 1.1
+ * Generates simple CAPTCHAs without any dependencies (no GD, no API).
+ * Uses 6MB of memory for 500x150 CAPTCHA on PHP 7, runs in about 10ms.
+ * @version 1.2
+ * @author abiusx
  */
-class PureCaptcha 
+class PureCaptcha
 {
-    protected $charWidth=6;
-    protected $charHeight=13;
-    protected $chars="2346789ABDHKLMNPRTWXYZ"; //do not modify!
-    protected $ascii="eNrtW0FuwyAQ/NIANjbOa3LMG6r8vWrrSonkkt0xDWDvIcGXEYHM7O6s4b
-    p4v3zcFlyuiwu/T/Hn4ba4h49fx7COwzqO3+P964tF+i0kViRWJLaQ4RGJF3PiETnQyFGzzidklC
-    A31znlkMjt7Zzb2+y/kjQ79MwEbEH/+kOftsjxLHKehK7cbYT/qu0KNBcHmosjzcVI63yi1znTyE
-    RHCAd6oZX479uL/yIuBho5SFgs5z8kc0YaOUm4iJfxXxVbEr23Djy0Dv+D1T/iXzvSyKjhop7/r+
-    M/LP5v83+w+qdM/aOP/6LKqXr9r0Lm+Z+H1uH/aPGf87/Y7X8t/jfA/2j8b43/MP/7Pv5P7frfbH
-    YPtKOszn8VcqIrxPrxXwetw/+5n/pfz395/Y8L2//HG+sf1ZwzjUw0Utf/byH+J+N/bf7L47/xv/
-    z7L7QnAFG+7NgAgDYAnRngHgog50wAXAcU9BtgaBqDEz3nTK/zVALw/QiAbwHxFvg/X4G53QJwuw
-    VgR4CCZYBc9wh0BpD3gKDuAVkJVE4Aw5EEgGICcF0JgG+C4vQCGI/QBTqWCT5cCSSDVhJANAH0Kw
-    AzwfsFMB3xHBzEJliFLHwPoMY5OBEy0cj8OWi0mAFmM8G1D0LwHgC0CYbaBIvaBB1mgHRuAajOEB
-    W+CcNnANGcM408UwnkYQLoTwBWApUTgN0F7vAuDNRdILsLvymA+ycgmwSd";
+    /**
+     * List of chars used in CAPTCHA. Should match the ascii table below.
+     * These characters are specifically selected as they are unambiguous.
+     * @var string
+     */
+    protected $chars="2346789ABDHKLNPRTWXYZ";
+
+    /**
+     * A compressed 13*6*len(chars) table of 0s and 1s for rendering ASCII text
+     * @var string
+     */
+    protected $ascii="eJztm0tuwzAMRK8U+ZOPc5ose4Yidy/ayEUkUGOOKle0rUWgLPrAWp6hS
+	NN5TJ2bPj+m0/0xuX7+dn59eX2cXzu/9n4d/Dr+rM/vP8wlu3fSvX06v/Z+Hfz6S/YopkMxh2xy
+	zCbP2eQlm7xmk7ds0kERYTRfRa7LRL12d6f/OGZAQv2fophr6j91nXr9w+uE+ofkTborKlLWv+p
+	CK+m/a/ovo39IFs7/7q7YW1H/KpLSP8j/1A2tpP++6X97+leRhfRPkTj/Y7SO/oft6D8geyZHpf
+	VPkWM2ec6+zguKqdd/6r8VY5aqfzi0jv7Hpv/t6h/urUr/y/m/QP2jQ+vo/7wH/Z8iUq9/ihTrH
+	xVZuP9VxaTqn4Ck6h9F/lddaCX9X5r+a+sfkk3/6+r/ak//KTLYabH+4fVPkbD+cYgU6x8Vqap/
+	nERS9X9a/9Rd2V79f9uT/lWkqv4XUxSl/4CE9T/codb/rjz/MjgAtmAA/gCAJNUA8weAGJNqgA9
+	7ADiDE+CjGCCVx/UGgCQ0ACRFA8TnlWjXDRrA4Aj4Pw3wTueNwFTkyiNgMaaqBW4lkMEZcDPAkg
+	FUZHsHTmUAg0NgFUkZICCpEihtAIqEJRAk9zAEm3PEbAC5eqpkAENT4DinxvuVbwBbPcDSdaZ7g
+	KW9TfcAFHmkt0CdoTFwM8BfT4C0ASjyUAYwNAfelgGonAp7gJlcNkBM8j1A3LGI5KFKIEODYIsG
+	CGIW7gHaCXA3YABDk2Aqv6neBBJ3unoJRJGiAVQptb4BOLTSL8EMTYJT5PIJQJGqnwKLZOHHoCq
+	SehUoIEUDqKwDH4PCmPgxKEZrGOD5BRevL54=";
+
     function __construct()
     {
         $this->ascii=unserialize(gzuncompress(base64_decode(
-                                                  preg_replace('/\s+/', '', $this->ascii))));
-        $this->text=$this->randomText();
-
+        	preg_replace('/\s+/', '', $this->ascii))));
     }
 
     /**
-     * Contains the captcha text.
-     * @var string
-     */
-    public $text;
-
-    /**
      * Generates random text for use in captcha
-     * @param  integer $length
-     * @return string
      */
     protected function randomText($length=4)
     {
         $res="";
         for ($i=0;$i<$length;++$i)
-            $res.=$this->chars[mt_rand(0,strlen($this->chars)-1)];
+            $res.=$this->chars[rand(0,strlen($this->chars)-1)];
         return $res;
     }
-    /**
-     * returns the index of a char in $chars array
-     */
-    protected function asciiEntry($char)
-    {
-        for ($i=0;$i<strlen($this->chars);++$i)
-            if ($this->chars[$i]==$char) return $i;
-        return -1;
 
-    }
     /**
-     * converts a text to a bitmap
-     * which is a 2D array of ones and zeroes denoting the text
-     */
-    protected function textBitmap($text,$spacing=2)
-    {
-        $width=$this->charWidth;
-        $height=$this->charHeight;
-        $result=array();
-        $baseY=$baseX=0;
-
-        for ($index=0;$index<strlen($text);++$index)
-        {
-            for ($j=0;$j<$height;++$j)
-            {
-                for ($i=0;$i<$width;++$i)
-                    $result[$baseY+$j][$baseX+$i]=
-                        1-$this->ascii[$this->asciiEntry($text[$index])][$j][$i];
-                for ($i=0;$i<$spacing;++$i)
-                    $result[$baseY+$j][$baseX+$width+$i]=0;
-            }
-            $baseX+=$width+$spacing;
-        }
-        return $result;
-    }
-    /**
-     * displays a bitmap string on the browser screen
+     * Displays a bitmap string on the browser screen
      */
     protected function displayBitmap($bitmap)
     {
         header("Content-Type: image/bmp");
         echo $this->bitmap2bmp($bitmap);
     }
+
     /**
-     * generates a monochrome BMP file
-     * a bitmap needs to be sent to this function
-     * i.e a 2D array with every element being either 1 or 0
-     * @param  integer $width
-     * @param  integer $height
-     * @param  array $bitmap
+     * Return width and height of bitmap
+     * @return array
+     */
+    private function bitmapDimensions($bitmap)
+    {
+    	return [count($bitmap[0]), count($bitmap)];
+    }
+
+    /**
+     * Generates a monochrome BMP file from a bitmap
+     * @param  array $bitmap 2D array with every element being either 1 or 0
      * @return string
      */
     protected function bitmap2bmp($bitmap)
     {
-        $width=count($bitmap[0]);
-        $height=count($bitmap);
+        list($width, $height)=$this->bitmapDimensions($bitmap);
         $bytemap=$this->bitmap2bytemap($bitmap);
 
         $rowSize=floor(($width+31)/32)*4;
-        $size=$rowSize*$height + 62; //62 metadata size
-        #bitmap header
-        $data= "BM"; //header
-        $data.= (pack('V',$size)); //bitmap size ,4 bytes unsigned little endian
+        $size=$rowSize*$height + 62; // 62 metadata size
+        # bitmap header
+        $data= "BM"; // header
+        $data.= (pack('V',$size)); // bitmap size, 4B unsigned LE
         $data.= "RRRR";
-        $data.= (pack('V',14+40+8)); //bitmap data start offset ,
-        //4 bytes unsigned little endian, 14 forced, 40 header, 8 colors
+        $data.= (pack('V',14+40+8)); // bitmap data start offset,
+        // 4 bytes unsigned little endian, 14 forced, 40 header, 8 colors
 
-        #info header
-        $data.= pack('V',40); //bitmap header size (min 40),
-        //4 bytes unsigned little-endian
-        $data.= (pack('V',$width)); //bitmap width , 4 bytes signed integer
-        $data.= (pack('V',$height)); //bitmap height , 4 bytes signed integer
-        $data.= (pack('v',1)); //number of colored plains , 2 bytes
-        $data.= (pack('v',1)); //color depth , 2 bytes
-        $data.= (pack('V',0)); //compression algorithm , 4 bytes (0=none, RGB)
-        $data.= (pack('V',0)); //size of raw data, 0 is fine for no compression
-        $data.= (pack('V',11808)); //horizontal resolution (dpi), 4 bytes
-        $data.= (pack('V',11808)); //vertical resolution (dpi), 4 bytes
-        $data.= (pack('V',0)); //number of colors in pallette (0 = all), 4 bytes
-        $data.= (pack('V',0)); //number of important colors (0 = all), 4 bytes
+        # info header
+        $data.= pack('V',40); // bitmap header size (min 40), 4B unsigned LE
+        $data.= pack('V',$width); // bitmap width, 4 bytes signed integer
+        $data.= pack('V',$height); // bitmap height, 4 bytes signed integer
+        $data.= pack('v',1); // number of colored plains, 2 bytes
+        $data.= pack('v',1); // color depth, 2 bytes
+        $data.= pack('V',0); // compression algorithm, 4 bytes (0=none, RGB)
+        $data.= pack('V',0); // size of raw data, 0 is fine for no compression
+        $data.= pack('V',11808); // horizontal resolution (dpi), 4 bytes
+        $data.= pack('V',11808); // vertical resolution (dpi), 4 bytes
+        $data.= pack('V',0); // number of colors in pallette (0 = all), 4 bytes
+        $data.= pack('V',0); // number of important colors (0 = all), 4 bytes
 
-        #color palette
-        $data.= (pack('V',0x00FFFFFF)); //next color, white
-        $data.= (pack('V',0)); //first color, black
+        # color palette
+        $data.= (pack('V',0x00FFFFFF)); // first color, white
+        $data.= (pack('V',0)); // second color, black
 
         for ($j=$height-1;$j>=0;--$j)
             for ($i=0;$i<$rowSize/4;++$i)
@@ -139,17 +112,15 @@ class PureCaptcha
                         $data.= pack('C',0);
         return $data;
     }
+
     /**
-     * Converts a bitmap to a bytemap, which is necessary for outputting it
-     *
+     * Converts a bitmap to a bytemap, necessary for conversion to BMP.
      */
-    protected function bitmap2bytemap($bitmap)
+    private function bitmap2bytemap($bitmap)
     {
-        $width=count($bitmap[0]);
-        $height=count($bitmap);
-        $bytemap=array();
+        list($width, $height)=$this->bitmapDimensions($bitmap);
+        $bytemap=[];
         for ($j=0;$j<$height;++$j)
-        {
             for ($i=0;$i<$width/8;++$i)
             {
                 $bitstring="";
@@ -160,21 +131,19 @@ class PureCaptcha
                         $bitstring.="0";
                 $bytemap[$j][]=bindec($bitstring);
             }
-        }
         return $bytemap;
     }
+
     /**
-     * rotates a bitmap, returning new dimensions with the bitmap
-     * return bitmap
+     * Rotates a bitmap, returning new dimensions with the bitmap
      */
     protected function rotateBitmap($bitmap, $degree)
     {
         $c=cos(deg2rad($degree));
         $s=sin(deg2rad($degree));
 
-        $width=count($bitmap[0]);
-        $height=count($bitmap);
-        $newHeight=round(abs($width*$s)+abs($height*$c));
+        list($width, $height)=$this->bitmapDimensions($bitmap);
+        $newHeight=round(abs($width*$s) + abs($height*$c));
         $newWidth=round(abs($width*$c) + abs($height*$s))+1;
         $x0 = $width/2 - $c*$newWidth/2 - $s*$newHeight/2;
         $y0 = $height/2 - $c*$newHeight/2 + $s*$newWidth/2;
@@ -189,48 +158,93 @@ class PureCaptcha
             }
         return $result;
     }
+
     /**
-     * scales a bitmap to be bigger
+     * Scale a bitmap based on scale factors
      */
-    protected function scaleBitmap($bitmap,$scaleX,$scaleY)
+    protected function scaleBy($bitmap, $scaleX, $scaleY)
     {
-        $width=count($bitmap[0]);
-        $height=count($bitmap);
-        $newHeight=$height*$scaleY;
-        $newWidth=$width*$scaleX;
+        list($width, $height)=$this->bitmapDimensions($bitmap);
+        return $this->scaleTo($bitmap, $width*$scaleX, $height*$scaleY);
+    }
+
+    /**
+     * Scale a bitmap to new width and height
+     */
+    protected function scaleTo($bitmap, $newWidth, $newHeight)
+    {
+        list($width, $height)=$this->bitmapDimensions($bitmap);
+        $scaleX=$newWidth/$width;
+        $scaleY=$newHeight/$height;
         $result=array_fill(0, $newHeight, array_fill(0, $newWidth, 0));
         for ($j=0;$j<$newHeight;++$j)
             for ($i=0;$i<$newWidth;++$i)
-                $result[$j][$i]=$bitmap[(int)($j/$scaleY)]
+                $result[$j][$i]=@$bitmap[(int)($j/$scaleY)]
                 [(int)($i/$scaleX)];
         return $result;
     }
+
     /**
-     * adds random noise to the captcha
+     * Merge multiple bitmaps into one
      */
-    protected function distort($bitmap,$noisePercent=5)
+    protected function mergeBitmaps(array $bitmap, $spacing = 0)
+    {
+    	if (empty($bitmap))
+    		return [];
+    	$res=$bitmap[0];
+    	for ($i=1;$i<count($bitmap);++$i)
+    		for ($y=0;$y<count($bitmap[$i]);++$y)
+    		{
+    			for ($_=0;$_<$spacing;++$_)
+	    			$res[$y][]=0; // spacing
+    			for ($x=0;$x<count($bitmap[$i][0]);++$x)
+    				$res[$y][]=$bitmap[$i][$y][$x];
+    		}
+    	return $res;
+    }
+
+    /**
+     * Adds random noise to a bitmap
+     */
+    protected function distort($bitmap, $noisePercentage)
     {
         for ($j=0;$j<count($bitmap);++$j)
             for ($i=0;$i<count($bitmap[0]);++$i)
-                if (isset($bitmap[$j][$i]) && mt_rand()%100<$noisePercent)
+                if (isset($bitmap[$j][$i]) && rand()%100<$noisePercentage)
                     $bitmap[$j][$i]=1;
         return $bitmap;
     }
 
     /**
-     * draw a captcha to the screen, returning its value
+     * Converts a string to a bitmap, rotating each character
      */
-    public function show($distort=true,$scale=2.3)
+    protected function textBitmap($text, $rotationDegrees = [0,0])
     {
-        $bitmap=$this->textBitmap($this->text);
-        $degree=mt_rand(2,4);
-        if (mt_rand()%100<50)
-            $degree=-$degree;
-        $bitmap=$this->rotateBitmap($bitmap,$degree);
-        $bitmap=$this->scaleBitmap($bitmap,$scale,$scale);
-        if ($distort) $bitmap=$this->distort($bitmap);
-        $this->displayBitmap($bitmap);
-        return $this->text;
+        $bitmap=[];
+    	for ($i=0;$i<strlen($text);++$i)
+    	{
+    		$bitmap[$i]=$this->ascii[strpos($this->chars, $text[$i])];
+        	$degree=rand($rotationDegrees[0],$rotationDegrees[1]);
+        	if (rand()%100<50)
+            	$degree=-$degree;
+            $bitmap[$i]=$this->scaleBy($bitmap[$i],5,5); // More clear letters.
+			$bitmap[$i]=$this->rotateBitmap($bitmap[$i], $degree);
+    		$bitmap[$i]=$this->scaleTo($bitmap[$i], 60, 65); // Uniform sizes.
+    	}
+    	return $this->mergeBitmaps($bitmap);
     }
 
+    /**
+     * Display captcha on the output, return its text
+     */
+    public function show($length = [4,5], $width = 500, $height = 150,
+    	$rotate = [1,35], $distort = [50,60])
+    {
+        $text=$this->randomText(rand($length[0],$length[1]));
+    	$bitmap=$this->textBitmap($text, $rotate);
+    	$bitmap=$this->scaleTo($bitmap, $width, $height);
+        $bitmap=$this->distort($bitmap, rand($distort[0],$distort[1]));
+        $this->displayBitmap($bitmap);
+        return $text;
+    }
 }
